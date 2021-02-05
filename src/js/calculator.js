82d7810699199432737adce61cf3a6a947222d5f
Connor Burgess <connorburgesscodes@gmail.com>
2021-02-05 09:06:15 -0800

    run green test in Jest for instantiation of Calculator object

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,6 +1,13 @@
 export default class Calculator {
   constructor() {
-    
-  }
+    this.age = 0;
+    this.lifeExpectancy = 0;
+    this.yearsLeft = 0;
+    this.pastExpectancy = ""
+    this.mercuryYears = 0;
+    this.venusYears = 0;
+    this.marsYears = 0;
+    this.jupiterYears = 0;
+    }
 
 }
